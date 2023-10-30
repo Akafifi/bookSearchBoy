8e2223d8a77e07419d20e8fa79a062ec42e5b90e
Akafifi <itsmeafifi@gmail.com>
2023-10-29 21:42:06 -0500

    defined schemas

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,27 @@
+export const typeDefs = `
+
+type Book {
+    _id: ID
+    authors: String
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+}
+
+type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [bookSchema]
+
+}
+
+type Query {
+    books: [Book]
+    users: [User]
+}
+
+`
